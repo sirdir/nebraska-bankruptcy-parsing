e23facb9ed266073b47facc18d8bae345aa6d6b4
Tymur Kubai aka sirdir <phddir@gmail.com>
2020-03-23 23:23:10 +0100

    small refactoring, segregate method

--- src/main/java/dev/sirdir/BankruptcyToCSV.java
+++ src/main/java/dev/sirdir/BankruptcyToCSV.java
@@ -12,6 +12,8 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BankruptcyToCSV {
 
@@ -25,36 +27,26 @@ public class BankruptcyToCSV {
         int iterations = (args.length == 1 && Integer.parseInt(args[0]) >= 10)
                 ? Integer.parseInt(args[0])
                 : 10;
-        parsing.doStuff(iterations);
+        parsing.runParser(iterations);
     }
 
-    private void doStuff(int iterations) throws IOException {
+    private void runParser(int iterations) throws IOException {
         try (CSVWriter csvWriter = new CSVWriter(new FileWriter(System.nanoTime() + "test.csv"))) {
-            csvWriter.writeNext(new String[]{"date","all","c7","c13"});
+            //headers
+            String[] row = {"date", "all", "c7", "c13"};
+            csvWriter.writeNext(row);
             for (int i = 1; i <= iterations; i++) {
                 String searchRelativeUrl = "/search/?query=bankruptcy+watch";
                 String initialUrl = createUrl(baseUrl, searchRelativeUrl, appendPagination, String.valueOf(i));
 
                 Document doc = Jsoup.connect(initialUrl).get();
-                Elements links = doc.select(".recommended__article-title > a");
-                for (Element link : links) {
-                    //skip if article not with stats
-                    if (!link.text().equalsIgnoreCase("bankruptcy watch")) continue;
+                Elements allArticles = doc.select(".recommended__article-title > a");
 
-                    Attributes attr = link.attributes();
-                    String href = attr.get("href");
-                    String articleUrl = createUrl(baseUrl, href);
-                    Document doc2 = Jsoup.connect(articleUrl).get();
-                    String dateStr = doc2.selectFirst(".most-recent-article-bi-line").text();
-                    //just cant parse full string because of "a.m." or "p.m." have dots
-                    //March 22, 2020 at 1:52 a.m.
-                    LocalDate date = LocalDate.parse(dateStr.split(" at")[0], dtf);
-                    Elements bankruptRows = doc2.select("[itemprop='articleBody'] > p");
-                    int all = bankruptRows.size();
-                    long c7 = bankruptRows.parallelStream().filter(el -> el.text().contains("Chapter 7")).count();
-                    long c13 = bankruptRows.parallelStream().filter(el -> el.text().contains("Chapter 13")).count();
-                    csvWriter.writeNext(new String[]{date.toString(), String.valueOf(all), String.valueOf(c7), String.valueOf(c13)});
-                }
+                //filter wrong articles
+                List<Element> filteredArticles = allArticles.parallelStream()
+                        .filter(el -> el.text().equalsIgnoreCase("bankruptcy watch"))
+                        .collect(Collectors.toList());
+                parseArticle(csvWriter, filteredArticles, row);
             }
         }
     }
@@ -65,5 +57,27 @@ public class BankruptcyToCSV {
             stringBuilder.append(part);
         }
         return stringBuilder.toString();
+    }
+
+    private void parseArticle(CSVWriter csvWriter, List<Element> links, String[] row) throws IOException {
+        for (Element link : links) {
+            Attributes attr = link.attributes();
+            String href = attr.get("href");
+            String articleUrl = createUrl(baseUrl, href);
+            Document article = Jsoup.connect(articleUrl).get();
+            String dateStr = article.selectFirst(".most-recent-article-bi-line").text();
+            //just cant parse full string because of "a.m." or "p.m." have dots
+            //March 22, 2020 at 1:52 a.m.
+            LocalDate date = LocalDate.parse(dateStr.split(" at")[0], dtf);
+            Elements bankruptRows = article.select("[itemprop='articleBody'] > p");
+            int all = bankruptRows.size();
+            long c7 = bankruptRows.parallelStream().filter(el -> el.text().contains("Chapter 7")).count();
+            long c13 = bankruptRows.parallelStream().filter(el -> el.text().contains("Chapter 13")).count();
+            row[0] = date.toString();
+            row[1] = String.valueOf(all);
+            row[2] = String.valueOf(c7);
+            row[3] = String.valueOf(c13);
+            csvWriter.writeNext(row);
+        }
     }
 }
